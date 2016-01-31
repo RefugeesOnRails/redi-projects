463afb8947a4b410c4bcdf225978a34b5a8859ba
Sebastian Probst Eide <sebastian@aircloak.com>
2016-01-31 14:36:47 +0100

    Order projects by votes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   before_action :authenticate_user, only: :mine
 
   def index
-    @projects = Project.all
+    @projects = Project.all.sort {|p| p.votes.count}.reverse
     respond_to do |format|
       format.json { render json: @projects.map(&:render_with_likes).to_json }
       format.html # shows index
